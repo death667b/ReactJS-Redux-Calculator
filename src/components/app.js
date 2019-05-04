4412ca2e0feb064b2462079f3b2ceb86ab04be53
death667b <idaniel@spinnys.com>
2019-05-04 16:45:24 +1000

    implement lazyloading js files

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import importedComponent from 'react-imported-component';
 
 import Home from './Home';
-import DynamicPage from './DynamicPage';
-import NoMatch from './NoMatch';
+import Loading from './Loading';
+
+const AsyncDynamicPage = importedComponent(
+  () => import(/* webpackChuckName: 'DynamicPage' */ './DynamicPage'),
+  { LoadingComponent: Loading }
+);
+
+const AsyncNoMatch = importedComponent(
+  () => import(/* webpackChunkName: 'NoMatch' */ './NoMatch'),
+  { LoadingComponent: Loading }
+);
 
 const App = () => {
   return (
@@ -11,8 +21,8 @@ const App = () => {
       <div>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route exaxt path='/dynamic' component={DynamicPage} />
-          <Route component={NoMatch} />
+          <Route exaxt path='/dynamic' component={AsyncDynamicPage} />
+          <Route component={AsyncNoMatch} />
         </Switch>
       </div>
     </Router>

aa364554d5b783591cd2a1b000b190bd020bec33
death667b <idaniel@spinnys.com>
2019-05-08 10:39:31 +1000

    Activates store in the root index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import store from './store/index';
+
+window.testStore = store;
 
 const rootEl = document.getElementById('root');
+const rootComponent = (
+  <Provider store={store}>
+      <App />
+    </Provider>
+);
 
-ReactDom.render(<App />, rootEl);
+ReactDom.render( rootComponent, rootEl);
 
 if (module.hot) {
   module.hot.accept();

aaa269ae996c4c964d5ec258ca6f65f1aa2f55ef
death667b <idaniel@spinnys.com>
2019-05-08 10:38:59 +1000

    Updates the home component to work with redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Layout from './Layout';
+import List from './List';
 
 const Home = () => {
   return (
@@ -10,6 +11,12 @@ const Home = () => {
       <p>
         <Link to='/dynamic'>Nav to dynamic</Link>
       </p>
+      <div className='row mt-5'>
+        <div className='col-md-4 offset-md-1'>
+          <h2>Articles</h2>
+          <List />
+        </div>
+      </div>
     </Layout>
   );
 };

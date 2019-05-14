321cbc8db33da9cf9b9a8c5dd0524efaa1dbfbab
death667b <idaniel@spinnys.com>
2019-05-14 13:57:45 +1000

    Create a redux action file

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,64 @@
-import { ADD_ARTICLE } from '../constants/action-types';
+import { 
+  ADD_ARTICLE, 
+  DATA_REQUESTED, 
+  PRESSED_CLEAR,
+  PRESSED_ALL_CLEAR,
+  VALUE_KEY_PRESSED,
+  EQUAL_KEY,
+  PLUS_OPERATOR_KEY_PRESSED,
+  MINUS_OPERATOR_KEY_PRESSED,
+  MULTIPLY_OPERATOR_KEY_PRESSED,
+  DIVIDE_OPERATOR_KEY_PRESSED,
+  PLUS_MINUS_OPERATOR_KEY_PRESSED,
+  SQUARED_OPERATOR_KEY_PRESSED
+} from '../constants/action-types';
 
 export function addArticle(payload) {
-  console.log('[REDUX] Added article: ', payload);
   return { type: ADD_ARTICLE, payload };
+}
+
+export function getData() {
+  return { type: DATA_REQUESTED };
+}
+
+//  Calculator
+
+export function clearEnterBox() {
+  return { type: PRESSED_CLEAR }
+}
+
+export function clearEverything() {
+  return { type: PRESSED_ALL_CLEAR }
+}
+
+export function valueKeyPress(payload) {
+  return { type: VALUE_KEY_PRESSED, payload };
+}
+
+export function EqualKey() {
+  return { type: EQUAL_KEY };
+}
+
+export function plusOperatorKey() {
+  return { type: PLUS_OPERATOR_KEY_PRESSED };
+}
+
+export function minusOperatorKey() {
+  return { type: MINUS_OPERATOR_KEY_PRESSED };
+}
+
+export function multiplyOperatorKey() {
+  return { type: MULTIPLY_OPERATOR_KEY_PRESSED };
+}
+
+export function divideOperatorKey() {
+  return { type: DIVIDE_OPERATOR_KEY_PRESSED };
+}
+
+export function plusminusOperatorKey() {
+  return { type: PLUS_MINUS_OPERATOR_KEY_PRESSED };
+}
+
+export function squaredOperatorKey() {
+  return { type: SQUARED_OPERATOR_KEY_PRESSED };
 }

f4099397a94b7b518942fb06921f2b1a88828163
death667b <idaniel@spinnys.com>
2019-05-14 13:59:14 +1000

    Link the route Calculator to the home compoment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import Layout from './Layout';
 import List from './List';
+import Post from './Posts';
 
 const Home = () => {
   return (
@@ -11,10 +12,17 @@ const Home = () => {
       <p>
         <Link to='/dynamic'>Nav to dynamic</Link>
       </p>
+      <p>
+        <Link to='/calculator'>Nav to Calculator</Link>
+      </p>
       <div className='row mt-5'>
         <div className='col-md-4 offset-md-1'>
           <h2>Articles</h2>
           <List />
+        </div>
+        <div className='col-md-4 offset-md-1'>
+          <h2>API Posts</h2>
+          <Post />
         </div>
       </div>
     </Layout>

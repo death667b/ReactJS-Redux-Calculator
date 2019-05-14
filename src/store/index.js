557d5c7a1d6704d90cdebd8e99ee429b899dd64f
death667b <idaniel@spinnys.com>
2019-05-14 13:58:14 +1000

    Create a redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
+import { 
+  forbiddenWordsMiddleware, 
+  processValueKey, 
+  processEqualKey,
+  processOperatorKey } from './middleware';
+import createSageMiddleware from 'redux-saga';
+import apiSaga from '../redux-sagas/api-saga';
 
-const store = createStore(rootReducer);
+const initSagaMiddleware = createSageMiddleware();
+
+// Enables redux dev tools on middleware
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  storeEnhancers(applyMiddleware(
+    forbiddenWordsMiddleware, 
+    processValueKey, 
+    processEqualKey,
+    processOperatorKey,
+    initSagaMiddleware))
+);
+
+initSagaMiddleware.run(apiSaga);
 
 export default store;
